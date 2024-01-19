e1bd0f706ffa63ce32721a94b4036688da87db43
Eric Philippot <EricpDev@gmail.com>
2024-01-18 21:55:54 -0600

    made some personal tweaks to the code

--- eShop React Demo/src/App.jsx
+++ eShop React Demo/src/App.jsx
@@ -1,14 +1,58 @@
-import './App.css'
-
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      image: "",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      image: "",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      image: "",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      image: "",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      image: "",
+    },
+  ];
 
   return (
-    <div>
-      <h1>Welcome to my new React Project.</h1>
-      <button>Test Button</button>
-    </div>
+    <main className="main__container">
+
+      <section className="main__categories-container">
+
+        {
+          categories.map(({id, title}) => {
+
+            return (
+
+              <div className="main__category-container" key={id}>
+                {/* image go here*/}
+                <div className="category__body-container">
+                  <h3>{title}</h3>
+                  <p>Shop Now</p>
+                </div>
+              </div>
+
+            );
+          })
+        }
+
+      </section>
+
+    </main>
   );
+};
 
-}
-
-export default App
+export default App;
