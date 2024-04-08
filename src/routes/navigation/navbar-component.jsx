b64c0f3e4c6bc3cdeaf91d58e7fcef8f311f9894
Eric Philippot <EricpDev@gmail.com>
2024-04-07 23:56:04 -0500

    added styled components to navbar

--- src/routes/navigation/navbar-component.jsx
+++ src/routes/navigation/navbar-component.jsx
@@ -6,7 +6,7 @@ import { signOutUser } from "../../utils/firebase/firebase-utils";
 import { CartIcon } from "../../components/cart-icon/cart-icon-component";
 import { CartDropdown } from "../../components/cart-dropdown/cart-dropdown-component";
 import logo from "../../assets/crown.svg";
-import "./navbar-styles.scss";
+import { LogoContainer, MainNavbar, NavbarMenuLinks, NavbarLink } from "./navbar-styles";
 
 
 
@@ -15,26 +15,26 @@ const NavBar = () => {
   const { currentUser } = useContext(UserContext);
   return (
     <Fragment>
-        <nav className="main__nav-bar">
+        <MainNavbar>
             <div className="nav__logo-container">
-                <Link className="nav__logo" to={"/"}>
+                <LogoContainer to={"/"}>
                     <img src={logo} alt="logo of a crown" />
-                </Link>
+                </LogoContainer>
             </div>
 
-            <ul className="nav__menu-links">
-                <li className="nav__menu-link"> <Link to={"/"}>HOME</Link> </li>
-                <li className="nav__menu-link"> <Link to={"/shop"}>SHOP</Link> </li>
-                <li className="nav__menu-link"> <Link to={"/contact"}>CONTACT US</Link> </li>
+            <NavbarMenuLinks>
+                <NavbarLink className="nav__menu-link"> <Link to={"/"}>HOME</Link> </NavbarLink>
+                <NavbarLink className="nav__menu-link"> <Link to={"/shop"}>SHOP</Link> </NavbarLink>
+                <NavbarLink className="nav__menu-link"> <Link to={"/contact"}>CONTACT US</Link> </NavbarLink>
                 {
                   currentUser 
-                  ? (<li onClick={signOutUser} className="nav__menu-link">SIGN OUT</li>) 
-                  : (<li className="nav__menu-link"> <Link to={"/account"}>SIGN IN</Link> </li>)
+                  ? (<NavbarLink onClick={signOutUser} className="nav__menu-link">SIGN OUT</NavbarLink>) 
+                  : (<NavbarLink className="nav__menu-link"> <Link to={"/account"}>SIGN IN</Link> </NavbarLink>)
                 }
                 <CartIcon/>
                 <CartDropdown />
-            </ul>
-        </nav>
+            </NavbarMenuLinks>
+        </MainNavbar>
         <Outlet/>
       </Fragment>
     )
---
+++ src/routes/navigation/navbar-styles.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const MainNavbar = styled.nav`
+  margin: 1.7rem 0;
+  padding: 0 1.7rem;
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 5rem;
+
+  img {
+    width: 110%;
+    height: 100%;
+  }
+`
+export const NavbarMenuLinks = styled.ul`
+  margin: 0;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  list-style: none;
+`
+
+export const NavbarLink = styled.li`
+  padding: 10px 15px;
+  white-space: nowrap;
+  cursor: pointer;
+  font-size: 1.1rem;
+  cursor: pointer;
+  transition: all 5ms ease-in;
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 4px;
+  }
+`
